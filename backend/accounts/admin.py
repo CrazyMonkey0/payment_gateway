6b69130cd1e17b1e5d2163ef66dd34021a4997c9
CrazyMonkey0 <hubcio1765@gmail.com>
2024-01-05 23:46:58 +0100

    Creation of models and admin panel in accounts app

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Profile model.
+
+    Attributes:
+    - fields (list): List of fields to be displayed and edited in the admin panel.
+    - list_display (list): List of fields to be displayed in the list view of the admin panel.
+    """
+    fields = ['user', 'api_key', 'bank_account']
+    list_display = ['user', 'user_email', 'api_key', 'bank_account']
+
+    def user_email(self, obj):
+        """
+        Returns the email of the associated user.
+        """
+        return obj.user.email
+
+    user_email.short_description = 'User Email'
