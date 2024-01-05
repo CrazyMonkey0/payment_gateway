a63e3ad3f83405fedf5645a86cb67d936e07aa83
CrazyMonkey0 <hubcio1765@gmail.com>
2024-01-06 00:02:26 +0100

    Using LoginView and LogoutView authentication views provided by django

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 
 urlpatterns = [
-
+    # Using authentication views provided by django
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # URL path to registration
     path('register/', views.register, name='register')
 ]
