4a9f989e7f45a4ae2dba794902870c738464043a
CrazyMonkey0 <hubcio1765@gmail.com>
2024-01-29 12:06:45 +0100

    Creation of an api

--- backend/payment_gateway/settings.py
+++ backend/payment_gateway/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 
     # Myapp
     'payments.apps.PaymentsConfig',
@@ -134,3 +135,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# Permission
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
