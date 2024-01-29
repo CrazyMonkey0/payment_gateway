da923b0249739181bd89ef1fcff82819339acb15
CrazyMonkey0 <hubcio1765@gmail.com>
2024-01-29 11:50:55 +0100

    Improving payment models

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -41,19 +41,9 @@ class OrderAdmin(admin.ModelAdmin):
     """
     Admin class for the Order model.
 
-    Fields:
-    - user
-    - client
-    - products
-    - total
-    - is_paid
-    List Display:
-    - user
-    - client
-    - total
-    - is_paid
-    - date_of_order
-    - date_of_payment
+    Attributes:
+    - fields (list): List of fields to be displayed and edited in the admin panel.
+    - list_display (list): List of fields to be displayed in the list view of the admin panel.
     """
     fields = ["user", "client", "products", "total", "is_paid",]
     list_display = ["user", "client", "total",
