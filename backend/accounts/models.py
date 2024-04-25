babb599f0cddd9f5c1ad65bb5a48e72fe5efcdb6
CrazyMonkey0 <hubcio1765@gmail.com>
2024-04-25 14:42:56 +0200

    Adding a new url_feedback field in the accounts model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,3 +6,4 @@ from django.core.validators import RegexValidator
 class Profile(AbstractUser):
     iban = models.CharField(max_length=32, default="", validators=[
                             RegexValidator(r'^[A-Z]{2}[0-9]*$')])
+    url_feedback = models.SlugField(default="")
