b14d98b3aa28449b4b1799aa15b88f604c4308c3
CrazyMonkey0 <hubcio1765@gmail.com>
2025-01-26 23:31:23 +0100

    Repairing the bank balance reset

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata data.json
+# Uncomment on when it was your first time firing up the app
+#python manage.py loaddata data.json
 
 django-admin makemessages -l en
 django-admin makemessages -l pl
