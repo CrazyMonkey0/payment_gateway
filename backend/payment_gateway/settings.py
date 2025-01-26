a207a37a3bae0966d01b33d969416a4b90913b4d
CrazyMonkey0 <hubcio1765@gmail.com>
2025-01-26 23:30:12 +0100

    Repair of card payment form error

--- backend/payment_gateway/settings.py
+++ backend/payment_gateway/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
       # CORS middleware
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
