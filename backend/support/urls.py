b8a65049f010e1c96223af93a254c15b39e80966
CrazyMonkey0 <hubcio1765@gmail.com>
2025-02-03 06:45:09 +0100

    Create room endpoint with POST method and validation for title

--- backend/support/urls.py
+++ backend/support/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('chat/', views.chats, name='chats'),
+    path('create-room/', views.create_room, name='create_room'),
 ]
--- backend/support/views.py
+++ backend/support/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .models import SupportRoom
 
 
@@ -12,3 +14,19 @@ def chats(request):
 
     # Przekazywanie pokoji do szablonu
     return render(request, 'support/chats.html', {'rooms': rooms})
+
+
+@csrf_exempt
+def create_room(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        room_title = data.get("title")
+
+        if not room_title:
+            return JsonResponse({"error": "Room title is required"}, status=400)
+
+        room = SupportRoom.objects.create(title=room_title, user=request.user)
+        return JsonResponse({"uuid": room.uuid, "title": room.title, "status": room.status}, status=201)
+    
+    else:
+        return HttpResponse("404")
