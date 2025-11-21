c47316fc0229ad23a3d60de6b4f84d054472bb60
CrazyMonkey0 <hubcio1765@gmail.com>
2025-11-21 13:22:14 +0100

    feat(accounts): add validator to url_feedback field in Profile

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,6 +4,10 @@ from django.core.validators import RegexValidator
 
 
 class Profile(AbstractUser):
-    iban = models.CharField(max_length=32, default="", validators=[
-                            RegexValidator(r'^[A-Z]{2}[0-9]*$')])
-    url_feedback = models.SlugField(default="")
+    iban = models.CharField(
+        max_length=32, default="", validators=[RegexValidator(r"^[A-Z]{2}[0-9]*$")]
+    )
+    url_feedback = models.SlugField(
+        default="",
+        validators=[RegexValidator(r"^[a-z0-9_-]+$", "Enter a valid slug.")],
+    )
